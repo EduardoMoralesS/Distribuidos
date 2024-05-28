01d845df7ec86c7ae805430c57fa814222a50ee9
EduardoMoralesS <72371256+EduardoMoralesS@users.noreply.github.com>
2024-05-28 17:47:00 -0600

    Update BD.py

--- BD.py
+++ BD.py
@@ -1,1 +1,58 @@
+import mysql.connector
+from mysql.connector import Error
+import random
+
+def otorgar_privilegios(host, user, password, user_to_grant, ip_nodo):
+    try:
+        connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            grant_privileges_query = f"""
+            GRANT ALL PRIVILEGES ON . TO '{user_to_grant}'@'{ip_nodo}' WITH GRANT OPTION;
+            FLUSH PRIVILEGES;
+            """
+            cursor.execute(grant_privileges_query)
+            connection.commit()
+            print(f"Todos los privilegios han sido otorgados al usuario '{user_to_grant}' desde la IP '{ip_address}'")
+    except Error as e:
+        print(f"Error al otorgar privilegios: {e}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("Conexión a la base de datos MySQL cerrada")
+
+# Parámetros de conexión
+host = 'localhost'  # Cambia esto según sea necesario
+user = 'root'
+password = '1234'
+user_to_grant = 'root'  # El usuario al que quieres otorgar privilegios
+ip_nodo = '192.168.30.132'  # La IP desde la cual el usuario se conectará
+
+# Otorgar privilegios
+otorgar_privilegios(host, user, password, user_to_grant, ip_nodo)
+
+host = 'localhost'  # Cambia esto según sea necesario
+user = 'root'
+password = '1234'
+user_to_grant = 'nodo1'  # El usuario al que quieres otorgar privilegios
+ip_nodo = '192.168.30.133'  # La IP desde la cual el usuario se conectará
+
+# Otorgar privilegios
+otorgar_privilegios(host, user, password, user_to_grant, ip_nodo)
+
+host = 'localhost'  # Cambia esto según sea necesario
+user = 'root'
+password = '1234'
+user_to_grant = ''  # El usuario al que quieres otorgar privilegios
+ip_nodo = '192.168.30.134'  # La IP desde la cual el usuario se conectará
+
+# Otorgar privilegios
+otorgar_privilegios(host, user, password, user_to_grant, ip_nodo)
+
 
